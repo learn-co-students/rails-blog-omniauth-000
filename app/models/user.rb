7e5466a2cc1f02088a5de2267f74b21effb9c14a
gjc2118 <gjc2118@gmail.com>
2015-07-22 23:08:43 -0700

    so sweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+
+  def self.create_with_omniauth(auth_hash)
+  self.create(provider: auth_hash[:provider],
+              uid: auth_hash[:uid],
+              name: auth_hash[:info][:name])
+end
+
   has_many :posts
   has_many :comments
 
