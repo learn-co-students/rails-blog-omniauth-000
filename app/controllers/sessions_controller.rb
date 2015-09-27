8ea1ea5fee318179131ae03b4fa64fe9a9ae47b4
garettarrowood <garettarrowood@gmail.com>
2015-09-27 17:48:03 -0400

    done."

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,20 @@
 class SessionsController < ApplicationController
 
+  def create
+    user = User.find_by_uid(auth_hash["uid"]) || User.create_with_omniauth(auth_hash)
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Signed in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
 end
