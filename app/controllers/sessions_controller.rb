76aef412823e5d6871df41fdda7d6cb96073a98d
NoneOfMaster <rcraigt@gmail.com>
2016-03-06 21:29:46 -0800

    funny omniauth pun

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,27 @@
 class SessionsController < ApplicationController
 
+  def new
+
+  end
+
+  def create
+    #binding.pry
+    user = User.find_or_create_from_auth_hash(auth_hash)
+    session[:user_id] = user.id
+    redirect_to user_path(:id => user.id) 
+    #need find_by_provider_and_uid
+    #set session
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+
+protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 RailsBlog::Application.routes.draw do
 
   root 'posts#index'
-  
+  #this is the redirect to github for the views '/auth/:provider', then it returns to the below
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
   resources :sessions
   resources :users, :only => [:new, :show, :index]
   resources :tags, :only => [:show, :index]
